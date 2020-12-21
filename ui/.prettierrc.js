9a569120d574b041e82177feb5c22bab031ccc63
Anton Repin <53556648+lehcode@users.noreply.github.com>
2020-12-21 10:54:56 +0200

    Fixed Angular SSR

--- ui/.prettierrc.js
+++ ui/.prettierrc.js
@@ -1,9 +1,36 @@
 module.exports = {
-	parser: 'typescript',
-	singleQuote: true,
-	semi: true,
-	tabWidth: 2,
-	useTabs: true,
-	trailingComma: 'none',
-	printWidth: 140
+  singleQuote: true,
+  semi: true,
+  tabWidth: 2,
+  useTabs: false,
+  trailingComma: "none",
+  printWidth: 140,
+  overrides: [
+    {
+      files: ["*.html"],
+      options: {
+        useTabs: true,
+        tabWidth: 4
+      }
+    },
+    {
+      files: ["*.js"],
+      options: {
+        singleQuote: false
+      }
+    },
+    {
+      files: ["*.ts"],
+      options: {
+        parser: "typescript",
+        singleQuote: false
+      }
+    },
+    {
+      files: ["*.json"],
+      options: {
+        singleQuote: false
+      }
+    }
+  ]
 };
