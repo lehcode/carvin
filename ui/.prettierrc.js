98ad145855f637aef774ace0f5c3840db181b52c
Anton Repin <53556648+lehcode@users.noreply.github.com>
2020-12-23 05:27:46 +0200

    Configured tests

--- ui/.prettierrc.js
+++ ui/.prettierrc.js
@@ -1,36 +1,49 @@
 module.exports = {
-  singleQuote: true,
-  semi: true,
-  tabWidth: 2,
-  useTabs: false,
-  trailingComma: "none",
-  printWidth: 140,
-  overrides: [
-    {
-      files: ["*.html"],
-      options: {
-        useTabs: true,
-        tabWidth: 4
-      }
-    },
-    {
-      files: ["*.js"],
-      options: {
-        singleQuote: false
-      }
-    },
-    {
-      files: ["*.ts"],
-      options: {
-        parser: "typescript",
-        singleQuote: false
-      }
-    },
-    {
-      files: ["*.json"],
-      options: {
-        singleQuote: false
-      }
-    }
-  ]
-};
+    singleQuote: false,
+    semi: true,
+    tabWidth: 2,
+    useTabs: false,
+    trailingComma: 'none',
+    printWidth: 140,
+    overrides: [
+        {
+            files: ['*.html'],
+            options: {
+                useTabs: true,
+                tabWidth: 4,
+            },
+        },
+        {
+            files: ['*.js'],
+            options: {
+                trailingComma: 'es5',
+                semi: true,
+                singleQuote: true,
+            },
+        },
+        {
+            files: ['*.ts'],
+            options: {
+                parser: 'typescript',
+                trailingComma: 'es5',
+                semi: true,
+            },
+        },
+        {
+            files: ['*.json'],
+            options: {
+                tabWidth: 4,
+                semi: true,
+                singleQuote: false,
+            },
+        },
+        {
+            files: ['*.yml'],
+            options: {
+                tabWidth: 4,
+                semi: true,
+                singleQuote: false,
+            },
+        },
+    ],
+}
