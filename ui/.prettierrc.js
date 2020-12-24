4e245c1414feade5d8022241c57ebe7d4ee6cff4
Anton Repin <53556648+lehcode@users.noreply.github.com>
2020-12-24 09:09:09 +0200

    Updated project configs

--- ui/.prettierrc.js
+++ ui/.prettierrc.js
@@ -1,49 +1,57 @@
 module.exports = {
-    singleQuote: false,
-    semi: true,
-    tabWidth: 2,
-    useTabs: false,
-    trailingComma: 'none',
-    printWidth: 140,
-    overrides: [
-        {
-            files: ['*.html'],
-            options: {
-                useTabs: true,
-                tabWidth: 4,
-            },
-        },
-        {
-            files: ['*.js'],
-            options: {
-                trailingComma: 'es5',
-                semi: true,
-                singleQuote: true,
-            },
-        },
-        {
-            files: ['*.ts'],
-            options: {
-                parser: 'typescript',
-                trailingComma: 'es5',
-                semi: true,
-            },
-        },
-        {
-            files: ['*.json'],
-            options: {
-                tabWidth: 4,
-                semi: true,
-                singleQuote: false,
-            },
-        },
-        {
-            files: ['*.yml'],
-            options: {
-                tabWidth: 4,
-                semi: true,
-                singleQuote: false,
-            },
-        },
-    ],
-}
+  singleQuote: false,
+  semi: true,
+  tabWidth: 2,
+  useTabs: false,
+  trailingComma: 'none',
+  printWidth: 140,
+  overrides: [
+    {
+      files: ['*.html'],
+      options: {
+        useTabs: true,
+        tabWidth: 4,
+      },
+    },
+    {
+      files: ['*.js'],
+      options: {
+        trailingComma: 'es5',
+        semi: true,
+        singleQuote: true,
+      },
+    },
+    {
+      files: ['*.ts'],
+      options: {
+        parser: 'typescript',
+        semi: true,
+        singleQuote: true,
+      },
+    },
+    {
+      files: ['*.json'],
+      options: {
+        tabWidth: 4,
+        semi: true,
+        singleQuote: false,
+      },
+    },
+    {
+      files: ['*.yml'],
+      options: {
+        tabWidth: 4,
+        semi: true,
+        singleQuote: false,
+      },
+    },
+    {
+      files: ['*.scss', '*.sass', '*.css'],
+      options: {
+        tabWidth: 2,
+        semi: true,
+        singleQuote: false,
+      },
+    },
+  ],
+};
