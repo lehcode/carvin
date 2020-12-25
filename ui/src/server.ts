9dfb3e26d1985389496ff64708a3d1059900ad4e
Anton Repin <53556648+lehcode@users.noreply.github.com>
2020-12-25 14:17:17 +0200

    Restored SSR

--- ui/src/server.ts
+++ ui/src/server.ts
@@ -1,7 +1,3 @@
-/***************************************************************************************************
- * Load `$localize` onto the global scope - used if i18n tags appear in Angular templates.
- */
-import '@angular/localize/init';
 import 'zone.js/dist/zone-node';
 
 import { ngExpressEngine } from '@nguniversal/express-engine';
@@ -13,19 +9,15 @@ import { APP_BASE_HREF } from '@angular/common';
 import { existsSync } from 'fs';
 
 // The Express app is exported so that it can be used by serverless Functions.
-// eslint-disable-next-line
 export function app(): express.Express {
   const server = express();
   const distFolder = join(process.cwd(), 'dist/ui/browser');
   const indexHtml = existsSync(join(distFolder, 'index.original.html')) ? 'index.original.html' : 'index';
 
   // Our Universal express-engine (found @ https://github.com/angular/universal/tree/master/modules/express-engine)
-  server.engine(
-    'html',
-    ngExpressEngine({
-      bootstrap: AppServerModule
-    })
-  );
+  server.engine('html', ngExpressEngine({
+    bootstrap: AppServerModule,
+  }));
 
   server.set('view engine', 'html');
   server.set('views', distFolder);
@@ -33,54 +25,34 @@ export function app(): express.Express {
   // Example Express Rest API endpoints
   // server.get('/api/**', (req, res) => { });
   // Serve static files from /browser
-  server.get(
-    '*.*',
-    express.static(distFolder, {
-      maxAge: '1y'
-    })
-  );
+  server.get('*.*', express.static(distFolder, {
+    maxAge: '1y'
+  }));
 
   // All regular routes use the Universal engine
-  server.get('*', (req: express.Request, res: express.Response) => {
+  server.get('*', (req, res) => {
     res.render(indexHtml, { req, providers: [{ provide: APP_BASE_HREF, useValue: req.baseUrl }] });
   });
 
   return server;
 }
 
-// eslint-disable-next-line
 function run(): void {
-  let port = 4000;
-  let schema = 'http';
-  let host = 'localhost';
-
-  if (process.env.NODE_ENV === 'test') {
-    // @ts-ignore
-    port = process.env.HTTP_PORT;
-    host = 'carvin.local';
-  }
-
-  if (process.env.NODE_ENV === 'production') {
-    // @ts-ignore
-    port = process.env.HTTPS_PORT;
-    schema = 'https';
-    host = 'carvin.biz';
-  }
+  const port = process.env.PORT || 4000;
 
   // Start up the Node server
   const server = app();
   server.listen(port, () => {
-    console.log(`Node Express server listening on ${schema}://${host}:${port}`);
+    console.log(`Node Express server listening on http://localhost:${port}`);
   });
 }
 
 // Webpack will replace 'require' with '__webpack_require__'
 // '__non_webpack_require__' is a proxy to Node 'require'
 // The below code is to ensure that the server is run only when not requiring the bundle.
-// eslint-disable-next-line
 declare const __non_webpack_require__: NodeRequire;
 const mainModule = __non_webpack_require__.main;
-const moduleFilename = (mainModule && mainModule.filename) || '';
+const moduleFilename = mainModule && mainModule.filename || '';
 if (moduleFilename === __filename || moduleFilename.includes('iisnode')) {
   run();
 }
