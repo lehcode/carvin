31329303113cf5e136e3d7777a097d48769e3625
lehcode <53556648+lehcode@users.noreply.github.com>
2021-01-09 19:53:47 +0200

    Updated tests

--- ui/e2e/src/app.e2e-spec.ts
+++ ui/e2e/src/app.e2e-spec.ts
@@ -1,32 +1,39 @@
-import {AppPage} from './app.po';
-import {browser, logging} from 'protractor';
+import { AppPage } from './app.po';
+import { browser, logging } from 'protractor';
+import { HarnessLoader } from '@angular/cdk/testing';
+import { ProtractorHarnessEnvironment } from '@angular/cdk/testing/protractor';
 
 describe('workspace-project App', () => {
   let page: AppPage;
+  let loader: HarnessLoader;
 
   beforeEach(() => {
     page = new AppPage();
+    loader = ProtractorHarnessEnvironment.loader();
   });
 
   it('should display hero title', async () => {
     await page.navigateTo();
     expect(await page.getHeroTitle())
-        .toEqual('Проверить VIN авто бесплатно');
+      .toEqual('Проверить VIN авто бесплатно');
   });
 
   it('should display hero lead', async () => {
     await page.navigateTo();
     expect(await page.getHeroLead())
-        .toEqual('Узнать историю автомобиля по коду VIN до покупки!');
+      .toEqual('Узнать историю автомобиля по коду VIN до покупки!');
   });
 
   afterEach(async () => {
     // Assert that there are no errors emitted from the browser
-    const logs = await browser.manage().logs()
-        .get(logging.Type.BROWSER);
+    const logs = await browser.manage()
+      .logs()
+      .get(logging.Type.BROWSER);
 
-    expect(logs).not.toContain(jasmine.objectContaining({
-      level: logging.Level.SEVERE,
-    } as logging.Entry));
+    expect(logs)
+      .not
+      .toContain(jasmine.objectContaining({
+        level: logging.Level.SEVERE,
+      } as logging.Entry));
   });
 });
--- ui/src/app/app.component.spec.ts
+++ ui/src/app/app.component.spec.ts
@@ -15,10 +15,4 @@ describe('AppComponent', () => {
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
   });
-
-  it(`should have a title 'RealFreeVIN.com :: Бесплатная расшифровка VIN-кода автомобиля'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('RealFreeVIN.com :: Бесплатная расшифровка VIN-кода автомобиля');
-  });
 });
