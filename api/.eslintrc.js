6bc902f7cd515190c56b04b1ff690d183fa93668
lehcode <53556648+lehcode@users.noreply.github.com>
2021-01-22 18:47:32 +0200

    Updated linter rules

--- api/.eslintrc.js
+++ api/.eslintrc.js
@@ -1,24 +1,64 @@
 module.exports = {
-  parser: '@typescript-eslint/parser',
-  parserOptions: {
-    project: 'tsconfig.json',
-    sourceType: 'module',
-  },
-  plugins: ['@typescript-eslint/eslint-plugin'],
-  extends: [
-    'plugin:@typescript-eslint/recommended',
-    'prettier/@typescript-eslint',
-    'plugin:prettier/recommended',
-  ],
   root: true,
-  env: {
-    node: true,
-    jest: true,
-  },
+  ignorePatterns: [
+    "dist",
+    "node_modules"
+  ],
+  plugins: [],
   rules: {
-    '@typescript-eslint/interface-name-prefix': 'off',
-    '@typescript-eslint/explicit-function-return-type': 'off',
-    '@typescript-eslint/explicit-module-boundary-types': 'off',
-    '@typescript-eslint/no-explicit-any': 'off',
+    "array-bracket-newline": [
+      "error",
+      { minItems: 2 }
+    ],
+    "array-element-newline": [
+      "error",
+      { minItems: 2 }
+    ],
+    "quote-props": [
+      "error",
+      "consistent-as-needed"
+    ],
+    "semi": "error"
   },
+  overrides: [
+    {
+      files: ["*.ts"],
+      parserOptions: {
+        project: ["tsconfig.json"],
+        createDefaultProgram: true
+      },
+      extends: [
+        "plugin:@typescript-eslint/recommended",
+        "prettier/@typescript-eslint"
+      ],
+      rules: {
+        "id-length": [
+          "warn",
+          { min: 2 }
+        ],
+        "newline-per-chained-call": [
+          "warn",
+          { ignoreChainWithDepth: 2 }
+        ],
+        "@typescript-eslint/ban-ts-comment": "off",
+        "@typescript-eslint/explicit-function-return-type": "warn",
+        "@typescript-eslint/explicit-module-boundary-types": "warn",
+        "@typescript-eslint/no-explicit-any": "warn",
+        "@typescript-eslint/quotes": [
+          "warn",
+          "single",
+          { allowTemplateLiterals: true }
+        ]
+      }
+    },
+    {
+      files: ["*.js"],
+      rules: {
+        quotes: [
+          "error",
+          "double"
+        ]
+      }
+    }
+  ]
 };
