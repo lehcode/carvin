a2493fe2e33c395ff36830ba2c0690f40fea4819
lehcode <53556648+lehcode@users.noreply.github.com>
2021-01-30 01:20:32 +0200

    Updated linter

--- api/.eslintrc.js
+++ api/.eslintrc.js
@@ -1,46 +1,34 @@
 module.exports = {
   root: true,
-  ignorePatterns: [
-    "dist",
-    "node_modules"
-  ],
+  ignorePatterns: ["dist", "node_modules"],
   plugins: [],
   rules: {
-    "array-bracket-newline": [
-      "error",
-      { minItems: 2 }
-    ],
-    "array-element-newline": [
-      "error",
-      { minItems: 2 }
-    ],
-    "quote-props": [
-      "error",
-      "consistent-as-needed"
-    ],
-    "semi": "error"
+    "indent": ["error",
+      2,
+      { SwitchCase: 1 }],
+    "array-bracket-newline": ["warn", "consistent"],
+    "array-element-newline": ["warn", "consistent"],
+    "function-call-argument-newline": ["warn", "consistent"],
+    "function-paren-newline": ["warn", "consistent"],
+    "quotes": ["error", "double"],
+    "quote-props": ["error", "consistent"],
+    "semi": "error",
   },
+  parser: "@typescript-eslint/parser",
   overrides: [
     {
       files: ["*.ts"],
       parserOptions: {
         project: ["tsconfig.json"],
-        createDefaultProgram: true
+        createDefaultProgram: true,
       },
       extends: [
-        "plugin:@typescript-eslint/recommended",
-        "prettier/@typescript-eslint"
+        "plugin:@typescript-eslint/recommended", "prettier/@typescript-eslint"
       ],
       rules: {
-        "array-bracket-newline": "off",
-        "id-length": [
-          "warn",
-          { min: 2 }
-        ],
-        "newline-per-chained-call": [
-          "warn",
-          { ignoreChainWithDepth: 2 }
-        ],
+        "id-length": ["warn", { min: 2 }],
+        "newline-per-chained-call": ["warn", { ignoreChainWithDepth: 2 }],
+        "quotes": ["error", "single"],
         "@typescript-eslint/ban-ts-comment": "off",
         "@typescript-eslint/explicit-function-return-type": "warn",
         "@typescript-eslint/explicit-module-boundary-types": "warn",
@@ -49,17 +37,8 @@ module.exports = {
           "warn",
           "single",
           { allowTemplateLiterals: true }
-        ]
-      }
+        ],
+      },
     },
-    {
-      files: ["*.js"],
-      rules: {
-        quotes: [
-          "error",
-          "double"
-        ]
-      }
-    }
-  ]
+  ],
 };
---
+++ api/.prettierrc.js
@@ -0,0 +1,29 @@
+module.exports = {
+  singleQuote: true,
+  semi: true,
+  tabWidth: 2,
+  useTabs: false,
+  trailingComma: 'none',
+  printWidth: 120,
+  overrides: [
+    {
+      files: ['*.js'],
+      options: {
+        trailingComma: 'es5',
+      },
+    },
+    {
+      files: ['*.ts'],
+      options: {
+        parser: 'typescript',
+      },
+    },
+    {
+      files: ['*.json'],
+      options: {
+        tabWidth: 4,
+        singleQuote: false,
+      },
+    },
+  ],
+};
