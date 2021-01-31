a8d943369270e4258fb61f2c3120f1ada0f0ab3f
lehcode <53556648+lehcode@users.noreply.github.com>
2021-01-31 20:49:19 +0200

    Added ConfigModule

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -4,19 +4,25 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { ApiModule } from './modules/api/api.module';
+import config from './config';
+
+debugger;
 
 @Module({
   imports: [
     ApiModule,
-    ConfigModule.forRoot(),
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [config],
+    }),
     MongooseModule.forRoot(
-      `mongodb://${process.env.MONGO_HOST}:${process.env.MONGO_PORT}/?authSource=admin&ssl=false`,
+      `mongodb://${process.env.MONGO_HOST}:${process.env.MONGO_PORT}/?${process.env.MONGO_PARAMS}`,
       {
         useNewUrlParser: true,
         useUnifiedTopology: true,
         useFindAndModify: false,
         useCreateIndex: true,
-        dbName: 'carvin',
+        dbName: process.env.MONGO_DB,
         user: process.env.MONGO_USER,
         pass: process.env.MONGO_PASS,
         family: 4,
