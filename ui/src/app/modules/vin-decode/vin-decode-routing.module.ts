6ac580a11d19b743b044815aa7c646ec340fd4e0
lehcode <53556648+lehcode@users.noreply.github.com>
2021-02-03 23:23:40 +0200

    Refactored free-report component location

--- ui/src/app/modules/vin-decode/vin-decode-routing.module.ts
+++ ui/src/app/modules/vin-decode/vin-decode-routing.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { VinDecodeComponent } from './vin-decode.component';
-import { FreeReportComponent } from './free-report/free-report.component';
+import { FreeReportComponent } from '../../components/free-report/free-report.component';
 
 const routes: Routes = [
   {path: '', component: VinDecodeComponent},
--- ui/src/app/modules/vin-decode/vin-decode.module.ts
+++ ui/src/app/modules/vin-decode/vin-decode.module.ts
@@ -3,7 +3,7 @@ import { CommonModule } from '@angular/common';
 
 import { VinDecodeRoutingModule } from './vin-decode-routing.module';
 import { VinDecodeComponent } from './vin-decode.component';
-import { FreeReportComponent } from './free-report/free-report.component';
+import { FreeReportComponent } from '../../components/free-report/free-report.component';
 
 
 @NgModule({
